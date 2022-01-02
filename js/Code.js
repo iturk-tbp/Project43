fbdc45b8b6861aaf24723552debc47b9e6efbb4e
iturk-tbp <78212324+iturk-tbp@users.noreply.github.com>
2022-01-02 18:34:32 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "DOBBY";
+const accessCode2 = "RON";
+const accessCode3 = "GRANGER";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("O D B Y B", 100,50)
+    fill("lightblue")
+    text("Hint: The house-elf who saved the golden trio from Malfoy Manor", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("O R N", 700,150)
+    fill("lightblue")
+    text("Hint: Harry Potter's best friend", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("R A G E N G R", 100,250)
+    fill("lightblue")
+    text("Hint: Hermione's last name", 100,270)
+
+}
