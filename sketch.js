ceeabb5d73db28ef9ab08b12733e32dd42e87edb
iturk-tbp <78212324+iturk-tbp@users.noreply.github.com>
2022-01-02 18:33:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var bg,bg2,form,system,code,security;
+var score=0;
+
+function preload() {
+  bg = loadImage("aladdin_cave.jpg");
+  bg2 = loadImage("treasure.jpg")
+}
+
+function setup() {
+  createCanvas(1000,500);
+  system = new System()
+  security = new Security()
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+  textSize(20);
+  fill("white");
+  text("Score: " + score, 450, 50);
+
+  if(score === 3) {
+    clear()
+    background(bg2)
+    fill("red")
+    textSize(40);
+    text("THE GALLEONS ARE YOURS, HARRY!",225, 200);
+  }
+
+  drawSprites()
+}
